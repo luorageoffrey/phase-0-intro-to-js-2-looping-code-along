b6a18eb7a8f79e5a57a22b01cd41cbfbc565a97d
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-08-22 18:50:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, msg) {
+
+    let message = [] 
+
+    for (let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${msg} gift!`)
+    }
+    return message;
+}
+
+
+
+
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
